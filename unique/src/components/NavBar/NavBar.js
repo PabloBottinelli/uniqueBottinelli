756430b41e0a6d112d225713f4b30f61b1fc7f05
Pablo Bottinelli <pablojulianb@gmail.com>
2021-11-16 14:22:51 -0300

    navbar

---
+++ unique/src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return(
+        <nav className='nav'>
+            <h1>UNIQUE</h1>
+            <a href='artesanias'>Artesanías</a>
+            <a href='joyas'>Joyas</a>
+            <a href='muebles'>Muebles</a>
+            <a href='3d'>3D</a>
+            <a href='otros'>Otros</a>
+        </nav>
+    )
+}
+
+export default NavBar
