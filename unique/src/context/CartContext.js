731247b2dcc0fbfcbbc645cb3484477d22602107
Pablo Bottinelli <pablojulianb@gmail.com>
2021-12-21 19:42:33 -0300

    CartContext

--- unique/src/context/CartContext.js
+++ unique/src/context/CartContext.js
@@ -4,17 +4,60 @@ const Context = React.createContext()
 
 export const CartContextProvider = ({children}) => {
     const [count, setCount] = useState(0)
-    const [product, setProducts] = useState([{}])
-    
-    const productos = []
+    const [product, setProducts] = useState([])
+    const [productQuantity, setProductQuantity] = useState([])
+
     const contador = count
+    const producto = product
 
     const setCart = (count, product) => {
-        const prod = productos.concat(product)
-        console.log(prod)
-        setCount(count + contador)
-        setProducts(prod)
-        console.log(product)
+        const isInCart = (id) => {
+            const filtro = producto.filter((product) => product.id == id)
+            if(filtro.length == 0){
+                const newQuantity = {
+                    id: product.id,
+                    quantity: count
+                }
+                setProductQuantity([...productQuantity, newQuantity])
+                setCount(count + contador)
+                setProducts([...producto, product])
+            }else{
+                const actualizarQuantity = (id) => {
+                    const encontrar = productQuantity.filter((product) => product.id == id)
+                    const cuenta = encontrar[0].quantity + count
+                    const eliminarQuantity = (id) => {
+                        const sinElProducto = productQuantity.filter((product) => product.id !== id)
+                        const newQuantity = {
+                            id: filtro[0].id,
+                            quantity: cuenta
+                        }
+                        setProductQuantity([...sinElProducto, newQuantity])
+                    }
+                    eliminarQuantity(filtro[0].id)
+                }               
+                actualizarQuantity(filtro[0].id)
+                setCount(count + contador)
+            }
+        }
+        isInCart(product.id)
+    }
+    // console.log(product)
+    // console.log(productQuantity)
+
+    const removeItem = (id) => {
+        const filtro = producto.filter((product) => product.id !== id)
+        const encontrar = productQuantity.filter((product) => product.id == id)
+        const eliminarQuantity = productQuantity.filter((product) => product.id !== id)
+        const cuenta = count - encontrar[0].quantity
+        setProducts(filtro)
+        setProductQuantity(eliminarQuantity)
+        setCount(cuenta)
+    }
+
+    const clear = () => {
+        setProducts([])
+        setProductQuantity([])
+        setCount(0)
     }
 
     return (
